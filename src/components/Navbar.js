d60583921825ad321fcf1e607d2562ff145a209b
Matege Billbright <125450966+BcodezMUJJE@users.noreply.github.com>
2024-06-24 14:03:35 +0000

    travify

---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import "./NavbarStyles.css";
+import { MenuItems } from "./MenuItems";
+
+class Navbar extends Component {
+  state = { clicked: false };
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo">Travify</h1>
+
+        <div className="menu-icons" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
+
+        <ul className="navbar-menu">
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  <i className={item.icon}></i>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+          <button>Sign Up</button>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
